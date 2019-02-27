2ec83dbc637b53a0e743931186e816b8f4f54abb
Edison Alba <edison.alba@chiper.co>
2019-02-27 11:11:35 -0500

    add new proyect

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Red Social Politecnico Gran Colombiano Proximamente!</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
